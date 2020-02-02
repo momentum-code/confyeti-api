d929cfebe55a5d7776660c3b4b3f9a8c224a5cf3
Mike Jancar <mikejancar@gmail.com>
2020-02-02 16:29:14 -0500

    Could not get any require mocking to work

--- users/tests/index.test.js
+++ users/tests/index.test.js
@@ -1,6 +1,4 @@
 var assert = require('assert');
-var proxyquire = require('proxyquire');
-var sinon = require('sinon');
 var users = require('../index');
 
 describe('The Users API', () => {
@@ -12,53 +10,4 @@ describe('The Users API', () => {
     assert.equal(response.statusCode, 400);
     assert.equal(responseBody.errorMessage, "Bad request");
   });
-
-  describe('when registering a user', () => {
-    it('should do so successfully with a properly given request', async () => {
-      const mockAws = {
-        CognitoIdentityServiceProvider: () => {
-          return {
-            signUp: (params) => {
-              return {
-                promise: async () => { }
-              };
-            }
-          };
-        }
-      };
-      const mockSignUpCall = proxyquire('aws-sdk', mockAws);
-
-      const clientId = 'blah';
-      const mockSharedFunc = {
-        getEnvironmentConfig: (env) => {
-          return {
-            cognitoClientId: clientId
-          };
-        }
-      };
-      const mockConfigCall = proxyquire('../opt/index', mockSharedFunc);
-
-      const email = 'blah@blah.com';
-      const password = 'blah123';
-
-      const testEvent = {
-        body: JSON.stringify({
-          Email: email,
-          Password: password
-        }),
-        httpMethod: 'POST',
-        requestContext: {
-          stage: 'Development'
-        }
-      };
-
-      const response = await users.handler(testEvent);
-
-      assert.ok(response, 'No response was returned');
-      assert.equal(response.statusCode, 200, 'statusCode was not 200');
-      assert.ok(response.body.successMessage, 'No successMessage was returned');
-    });
-  });
-
-  afterEach(() => sinon.restore());
 });
